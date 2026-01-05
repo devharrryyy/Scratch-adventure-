aabf0834748e610c43ba2fc6315982288f5cafb5
devharrryyy <shaizan762002@gmail.com>
2026-01-05 23:49:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,35 +19,11 @@ app.get("/", (req, res) => {
 });
 
 const dares = [
-  "Apni ek cute selfie bhejo",
-  "Sirf emojis me apna mood batao",
-  "Ek honest compliment bhejo",
-  "Apna favourite song share karo",
-  "5 minute tak fast reply karo",
-  "Ek flirty line likho",
-  "Apna nickname batao",
-  "Ek cheez batao jo tumhe smile de",
-  "Voice note me sirf ‘hi’ bolo",
-  "Apni recent photo bhejo",
-  "Ek secret emoji me likho",
-  "Apni playlist ka last song batao",
-  "Ek fun GIF bhejo",
-  "Apna current status describe karo",
-  "Ek random memory share karo",
-  "Sirf ek word me mujhe describe karo",
-  "Apni favourite app batao",
-  "Ek funny sticker bhejo",
-  "Apna favourite late-night activity batao",
-  "Ek sweet good-night line likho",
-  "Apni handwriting me naam likh ke pic bhejo",
-  "Ek imaginary date idea batao",
-  "Apna screen wallpaper describe karo",
-  "Ek cheez jo tumhe attractive lagti ho",
-  "Sirf ‘yes’ ya ‘no’ me next sawaal ka jawab do",
-  "Ek photo jo tumhe pasand ho share karo",
-  "Apni favourite gaali censored me likho",
-  "Ek inside joke banao",
-  "Apna mood change karne wali cheez batao",
+  "Apni ek cute selfie bhejo", "Sirf emojis me apna mood batao", "Ek honest compliment bhejo",
+  "Apna favourite song share karo", "5 minute tak fast reply karo", "Ek flirty line likho",
+  "Apna nickname batao", "Ek cheez batao jo tumhe smile de", "Voice note me sirf 'hi' bolo",
+  "Ek secret emoji me likho", "Apni playlist ka last song batao", "Ek fun GIF bhejo",
+  "Apna current status describe karo", "Ek random memory share karo", "Ek imaginary date idea batao"
 ];
 
 const rooms = {};
@@ -93,19 +69,15 @@ io.on("connection", socket => {
 
   socket.on("scratch-complete", room => {
     if (!rooms[room] || rooms[room].scratched) return;
-    
     rooms[room].scratched = true;
     io.to(room).emit("reveal", rooms[room].dare);
   });
 
   socket.on("done", room => {
     if (!rooms[room]) return;
-    
-    // Switch turn and get new dare
     rooms[room].scratched = false;
     rooms[room].turn = (rooms[room].turn === 'creator') ? 'joiner' : 'creator';
     rooms[room].dare = getRandomDare();
-    
     io.to(room).emit("new-turn", {
       dare: rooms[room].dare,
       turn: rooms[room].turn
@@ -114,19 +86,7 @@ io.on("connection", socket => {
 
   socket.on("disconnect", () => {
     console.log("❌ User disconnected:", socket.id);
-    
-    // Clean up rooms when creator leaves
-    for (const room in rooms) {
-      if (rooms[room].creator === socket.id) {
-        io.to(room).emit('room-closed');
-        delete rooms[room];
-        console.log(`🗑️ Room ${room} deleted`);
-      } else if (rooms[room].joiner === socket.id) {
-        rooms[room].joiner = null;
-        rooms[room].joinerJoined = false;
-        io.to(room).emit('joiner-left');
-      }
-    }
+    // Room will be cleaned up when creator leaves
   });
 });
 
