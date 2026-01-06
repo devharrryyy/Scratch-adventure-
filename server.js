a9b5396e03057165a3ac291514e18bfe3a59550d
devharrryyy <shaizan762002@gmail.com>
2026-01-07 03:12:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,44 +16,7 @@ app.use(express.static(__dirname));
 app.get("/", (_, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 const dares = [
-  "Apni ek cute selfie bhejo", "Sirf emojis me apna mood batao", "Ek honest compliment do mujhe",
-  "Apna favourite song share karo", "5 minute tak fast reply karo", "Ek flirty line likho",
-  "Apna nickname batao", "Ek cheez batao jo tumhe smile deti h", "Voice note me sirf 'hey daddy' bolo😁",
-  "Ek secret btao jo koi ni janta tunhre alava", "Apni bra strap ki snap bhejo",
-  "Ek apna fun GIF bhejo", "Apna relationship status describe karo", "Ek random memory share karo",
-  "Ek imaginary date idea batao", "Kisi ko tag karke unki taarif karo (story me)",
-  "Apna favourite person batao", "Ek dark joke sunao", "kabhi ghar se paise chori kre hai ?",
-  "Apne hand se boobs hide krke photo bhejo(Naked)", "Ek song ki ek line gaake sunao", "Apni favourite movie batao",
-  "Ek childhood story share karo", "Apna hidden talent dikhao", "following me se kisi random ko propose kro",
-  "Apna favourite food batao", "Ek fake love story banao", "Apna funny face bna kr pic bhejo",
-  "Ek compliment khud ko do", "Ek joke sunao", "mummy se bolo mujhe love marriage krni hai(video bhejo)",
-  "Ek dream vacation spot batao", "Apna favourite game batao", "Ek weird habit share karo",
-  "Apna favourite flower batao", "Ek motivational quote bhejo", "Apna favourite movie genre batao",
-  "Ek childhood dream batao", "Mirro video bnao apne back body ki(NAKED)", "Ek superpower chuno",
-  "Apna favourite cartoon batao", "Ek magic trick dikhao", "Apna favourite sport bato",
-  "Ek tongue twister bolo", "Apna favourite festival bato", "Ek random fact share karo",
-  "Apna favourite app batao", "Ek mimicry karo", "Apna favourite dessert batao",
-  "Ek bucket-list item share karo", "Apna favourite subject batao", "Ek 1 min dance karo or video bana ke bhejo",
-  "sexy song pr bra & penty me dance kro(video)", "Apna bf/gf ki pic send kro", "Ek funny nickname do mujhe",
-  "Apna favourite time of day batao", "Ek dream job batao", "Apni naked thighs ki snap bhejo (cross leds)",
-  "Ek random compliment do random ko", "Apna favourite weather batao", "Ek secret talent reveal karo",
-  "Apna favourite quote batao", "Ek virtual hug bhejo", "Apna favourite place in home batao",
-  "Ek imaginary pet name batao", "Apna favourite candy batao", "Ek 3-word story likho",
-  "Apna favourite drink batao", "Ek childhood game yaad karo", "Apna favourite song mood batao",
-  "Ek dream car name btao", "Apna favourite TV show batao", "Ek random act of kindness karo",
-  "Apna phon number batao", "Ek kiss bhejo video me", "Apna favourite scent memory batao",
-  "Ek nightmare share karo", "tunhre kitne fake account hai batao", "Ek 15 second singing clip bhejo",
-  "Apna favourite ice-cream flavour batao", "Ek friend ka naam batao jiske liye feeling hai",
-  "tumhra body count btao", "Ek random dance move bhejo (video me)", "Apna favourite breakfast batao",
-  "Ek childhood fear share karo", "ek snap kro pic penty or bra me", "tunhra childhood trauma share kro",
-  "green chillies khankr vudeo me reaction dikhao", "tumhra dream house kaisa hoga ?",
-  "kitne logo ko cheat kiya hai ?", "Ek random post pr author ko galiya do", "Apna favourite pizza topping batao",
-  "kisi friend ki story pr like or comment kro(ganda)", "kisi ek friend ko galiya dekr block kro",
-  "Ek pickup line likho", "bf/gf kaisa chahiye ?", "Ek childhood real life superhero batao",
-  "Apna favourite midnight snack batao", "Ek virtual kiss bhejo", "Apna weight btao",
-  "online chocolate order kro mere liye", "Apna favourite childhood memories batao", "Ek devil laugh audio bhejo",
-  "Apni real age batao", "Ek favourite planet name batao jaha jana chahte ho", "Apna favourite sound of nature batao",
-  "Ek 15 second actor ki mimicry karo (video me)", "Apna apne naked hips ki snap bhejo", "jab tum sad/upset hote ho to kya krna psnd krte ho"
+  "Apni ek cute selfie bhejo", "naked thighs snap send kro", "Ek honest compliment do mujhe", "Apna favourite song share karo", "5 minute tak fast reply karo", "Ek flirty line likho", "Apna nickname batao", "Ek cheez batao jo tumhe smile deti h", "Voice note me sirf 'hey daddy' bolo", "Ek secret btao jo koi ni janta tumhre alava", "Apni playlist ka last song batao", "Ek apna fun GIF bhejo", "Apna relationship status describe karo", "Ek random memory share karo", "Ek imaginary date idea batao", "mujhko tag karke meri taarif karo (story me)", "Apna favourite person batao", "Ek dark joke sunao", "kabhi ghar se paise chori kre hai ?", "full body snap send kro bra & penty me", "Ek song ki ek line gaake sunao", "Apni favourite movie batao", "Ek childhood story share karo", "Apna hidden talent dikhao", "following me se kisi random ko propose kro", "Apna favourite food batao", "fuck me daddy bolo voice note me(sexy way)", "Apna funny face bna kr pic bhejo", "Mujhse ek compliment do", "apne sare hidden mole dikhao", "mummy se bolo mujhe love marriage krni hai(video bhejo)", "Ek dream vacation spot batao", "Apna favourite game batao", "Ek weird habit share karo", "Apna favourite flower batao", "Hands se boobs cover krke snap bhejo(naked)", "Mirror me back body snap bhejo (Naked)", "Ek childhood dream batao", "apni pussy hands se cover kro snap bhejo", "Ek superpower chuno", "Apna favourite cartoon batao", "sexy song pr dance krke snap bhejo bra & penty me", "Apna favourite sport batao", "Ek tongue twister bolo", "Apna favourite festival batao", "Ek random fact share karo", "Apna favourite app batao", "Ek mimicry karo", "Apna favourite dessert batao", "Apna dick dikhao", "Apna favourite subject batao", "Ek 1 min dance karo or video bana ke bhejo", "Apna childhood photo send karo", "Apna bf/gf ki pic send kro", "Ek cute nickname do mujhe", "Apna favourite time of day batao", "Ek dream job batao", "apna dick dikhao", "Ek random compliment do random ko", "apna dick dikhao", "Ek secret talent reveal karo", "Apna favourite quote batao", "ek naked pic send kro apni", "Apna favourite place in home batao", "apne hips par slap krke snap bhejo(video)", "Apna favourite candy batao", "Apna favourite drink batao", "Ek childhood game yaad karo", "Apna favourite song mood batao", "Ek dream car name btao", "Apna favourite TV show batao", "Ek random act of kindness karo", "Apna phon number batao", "Ek kiss bhejo video me", "Apna favourite scent memory batao", "Ek nightmare share karo", "tunhre kitne fake account hai batao", "Ek 15 second singing clip bhejo", "Apna favourite ice-cream flavour batao", "Ek friend ka naam batao jiske liye feeling hai", "tumhra body count btao", "Ek random dance move bhejo (video me)", "Apna favourite breakfast batao", "Ek childhood fear share karo", "Apna favourite meme batao", "tunhra childhood trauma share kro", "green chillies khankr vudeo me reaction dikhao", "tumhra dream house kaisa hoga ?", "kitne logo ko cheat kiya hai ?", "Ek random post pr author ko galiya do", "Apna favourite pizza topping batao", "kisi friend ki story pr like or comment kro(ganda)", "kisi ek friend ko galiya dekr block kro", "Ek pickup line likho", "bf/gf kaisa chahiye ?", "Ek childhood real life superhero batao", "Apna favourite midnight snack batao", "Ek virtual kiss bhejo", "Apna weight btao", "online chocolate order kro mere liye", "Apna favourite childhood memories batao", "Ek devil laugh audio bhejo", "Apni real age batao", "Ek favourite planet name batao jaha jana chahte ho", "Apna favourite sound of nature batao", "Ek 15 second actor ki mimicry karo (video me)", "Apna favourite sppt btao", "jab tum sad/upset hote ho to kya krna psnd krte ho"
 ];
 
 const rooms = {};
